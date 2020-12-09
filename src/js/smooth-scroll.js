f85984bbc45cf7c96f2b703cc384e815cfe6a222
kensuke0518 <k.com.play@gmail.com>
2020-12-10 00:06:13 +0900

    iOSでアンカーリンクスクロールのアニメーションがうまく動かない点を修正

--- src/js/smooth-scroll.js
+++ src/js/smooth-scroll.js
@@ -1,4 +1,25 @@
 module.exports.func = () => {
+    const a = document.getElementsByTagName('a');
+    for (let i = 0; i < a.length; i++) {
+        a[i].addEventListener('click', e => {
+            let href = a[i].getAttribute('href');
+            let pagename = window.location.href;
+            pagename = pagename.replace(/#.*/, '');
+            href = href.replace(pagename, '');
+            if (href.search(/^#/) >= 0){
+                e.preventDefault();
+                const target = e.target;
+                const targetId = target.hash
+                document.querySelector(targetId).scrollIntoView({
+                    behavior: "smooth",
+                    block: "start"
+                })
+                return false;
+            }
+        })
+    }
+}
+/*module.exports.func = () => {
     const a = document.querySelectorAll('a[href^="#"]');
     for (let i = 0; i < a.length; i++) {
         a[i].addEventListener('click', e => {
@@ -12,4 +33,4 @@ module.exports.func = () => {
         })
         return false;
     }
-}
+}*/
